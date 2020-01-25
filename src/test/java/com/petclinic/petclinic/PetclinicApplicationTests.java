45f7095ea03749d6b709c6f4ce46c21d9f8cd921
enesoral <oralenes0@gmail.com>
2020-01-25 19:30:35 +0300

    Initial Project Add

---
+++ src/test/java/com/petclinic/petclinic/PetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petclinic.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
