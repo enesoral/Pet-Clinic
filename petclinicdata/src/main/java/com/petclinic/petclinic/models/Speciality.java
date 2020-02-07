027d0bb3bb6b63d1411826d1f7c2772529266be8
enesoral <oralenes0@gmail.com>
2020-02-07 13:49:26 +0300

    Adding speciality entity and added to vet. Closes #24

---
+++ petclinicdata/src/main/java/com/petclinic/petclinic/models/Speciality.java
@@ -0,0 +1,14 @@
+package com.petclinic.petclinic.models;
+
+public class Speciality extends BaseEntity {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- petclinicdata/src/main/java/com/petclinic/petclinic/models/Vet.java
+++ petclinicdata/src/main/java/com/petclinic/petclinic/models/Vet.java
@@ -1,4 +1,16 @@
 package com.petclinic.petclinic.models;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
