06cef9ac568f25c2d2b4bf665b76191101f4a523
enesoral <oralenes0@gmail.com>
2020-02-07 13:37:53 +0300

    Adding visit entity, updating owner with pet relations. Closes #23

--- petclinicdata/src/main/java/com/petclinic/petclinic/models/Owner.java
+++ petclinicdata/src/main/java/com/petclinic/petclinic/models/Owner.java
@@ -1,4 +1,16 @@
 package com.petclinic.petclinic.models;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
---
+++ petclinicdata/src/main/java/com/petclinic/petclinic/models/Visit.java
@@ -0,0 +1,38 @@
+package com.petclinic.petclinic.models;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity {
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public Visit() {
+        this.date = LocalDate.now();
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
