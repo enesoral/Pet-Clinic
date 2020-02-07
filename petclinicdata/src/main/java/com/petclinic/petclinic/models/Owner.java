9a2fe583fd0acac50f11b5792599f3f32529d8f7
enesoral <oralenes0@gmail.com>
2020-02-07 13:56:46 +0300

    Adding contact info properties to owner. Closes #25

--- petclinicdata/src/main/java/com/petclinic/petclinic/models/Owner.java
+++ petclinicdata/src/main/java/com/petclinic/petclinic/models/Owner.java
@@ -4,7 +4,34 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+    private String telephone;
+    private String address;
+    private String city;
     private Set<Pet> pets;
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 
     public Set<Pet> getPets() {
         return pets;
